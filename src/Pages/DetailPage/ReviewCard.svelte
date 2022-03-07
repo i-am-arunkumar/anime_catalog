2b96adf73dc99ee2ff3b95815cad116db9397f74
LavilleVishnuPrasath <curryvishnu@gmail.com>
2022-03-07 11:34:02 +0530

    bug fix

--- src/Pages/DetailPage/ReviewCard.svelte
+++ src/Pages/DetailPage/ReviewCard.svelte
@@ -1,7 +1,7 @@
 <script>
   export let data;
-  let dt = new Date(data.date)
-  let showMore = false   
+  let dt = new Date(data.date);
+  let showMore = false;
 </script>
 
 <div class="card m-4">
@@ -17,17 +17,22 @@
       </div>
       <div class="media-content">
         <p class="title is-4">{data.user.username}</p>
-        <time datetime="2016-1-1">{ dt.toLocaleDateString() + " " + dt.toLocaleTimeString()}</time>
+        <time datetime="2016-1-1"
+          >{dt.toLocaleDateString() + " " + dt.toLocaleTimeString()}</time
+        >
+      </div>
     </div>
-    </div>
- 
+
     <div class={`content ${showMore ? "" : "show-less"}`}>
       {data.review}
-    </div> 
-    <button class="button is-text" on:click={()=>{
-      showMore = !showMore 
-    }}>
-      {showMore ? "show less": "show more"}
+    </div>
+    <button
+      class="button is-text"
+      on:click={() => {
+        showMore = !showMore;
+      }}
+    >
+      {showMore ? "show less" : "show more"}
     </button>
     <div class="card-footer-item ">
       <div class="level" style="width: 100%;">
@@ -46,14 +51,13 @@
   </div>
 </div>
 
-
 <style>
-     .show-less{
-        display: -webkit-box;
-        -webkit-line-clamp: 5;
-        max-height: 150px;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    } 
+  .show-less {
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    max-height: 150px;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 </style>
